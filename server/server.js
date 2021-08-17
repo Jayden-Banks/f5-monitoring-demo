08a971686a94c9800d1932dc3d00a51fce20181b
Jayden Banks <jayden.banks21@gmail.com>
2021-08-17 09:45:12 -0600

    rollbar

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ const app = express()
 const ctrl = require('./controller.js')
 const port = process.env.PORT || 4040
 
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+    accessToken: '834c8deadb0e46698d757e7e14dc6d22',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+
+
 app.get('/', ctrl.main)
 
 
