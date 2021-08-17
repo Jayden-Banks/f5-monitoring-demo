bb9fcbf8cd0830dd0d28865454733e05e94e4319
Jayden Banks <jayden.banks21@gmail.com>
2021-08-17 09:47:33 -0600

    rollbar

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,18 @@
 const path = require('path')
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+    accessToken: '834c8deadb0e46698d757e7e14dc6d22',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+
+
+
 
 module.exports = {
     main: (req,res) => {
+        rollbar.log('Hello world!')
         res.sendFile(path.join(__dirname, '../public/index.html'))
     }
 }
--- server/server.js
+++ server/server.js
@@ -3,12 +3,7 @@ const app = express()
 const ctrl = require('./controller.js')
 const port = process.env.PORT || 4040
 
-const Rollbar = require('rollbar')
-const rollbar = new Rollbar({
-    accessToken: '834c8deadb0e46698d757e7e14dc6d22',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-})
+
 
 
 
